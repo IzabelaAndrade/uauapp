fc3d40286c5068da4142300ed6822a13c5e0a3a1
Igor Vinicius <sir.vini777@gmail.com>
2019-12-06 17:10:43 -0400

    add redux and reactotron

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 
+import './src/config/ReactotronConfig';
+
+import { store, persistor } from './src/store';
 import Routes from './src/routes';
-
-import store from './src/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="light-content" backgroundColor="#f48024" />
-      <Routes />
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#f48024" />
+        <Routes />
+      </PersistGate>
     </Provider>
   );
 }
-
